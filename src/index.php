7e7b7b100184ba4654e63702e0ad89c6c6913961
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2018-11-12 13:28:28 +0600

    basic setup

--- src/index.php
+++ src/index.php
@@ -30,31 +30,95 @@
                         <img src="loading.gif" alt="loading.." class="d-none">
                     </button>
                 </form>-->
-                
+
                 <div id="indicator" class="d-flex justify-content-center py-3">
-                    <div class="indicator mx-1 rounded-circle border border-secondary bg-secondary"></div>
-                    <div class="indicator mx-1 rounded-circle border border-secondary bg-secondary bg-transparent"></div>
-                    <div class="indicator mx-1 rounded-circle border border-secondary bg-secondary bg-transparent"></div>
+                    <div class="mx-1 rounded-circle border border-secondary bg-secondary bg-transparent" :id="'indicator-' + stage.lang"></div>
+                    <div class="mx-1 rounded-circle border border-secondary bg-secondary bg-transparent" :id="'indicator-' + stage.intro"></div>
+                    <div class="mx-1 rounded-circle border border-secondary bg-secondary bg-transparent" :id="'indicator-' + stage.main"></div>
                 </div>
-                <div class="card bg-light mb-3">
-                    <div class="card-header">Select Language</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Would you like to speak in English o en Espanol?</h5>
-                        <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline1" name="customRadioInline1"
-                                   class="custom-control-input">
-                            <label class="custom-control-label" for="customRadioInline1">English</label>
-                        </div>
-                        <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline2" name="customRadioInline1"
-                                   class="custom-control-input">
-                            <label class="custom-control-label" for="customRadioInline2">Spanish</label>
+
+                <form class="form-inline">
+                    <div :id="stage.lang" class="w-100" :style="{display: current_stage === stage.lang ? 'block' : 'none'}">
+                        <div class="card bg-light mb-3">
+                            <div class="card-header">Select Language</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Would you like to speak in English o en Espanol?</h5>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="form_lang1" name="form_lang" class="custom-control-input" checked="checked">
+                                    <label class="custom-control-label" for="form_lang1">English</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="form_lang2" name="form_lang" class="custom-control-input">
+                                    <label class="custom-control-label" for="form_lang2">Spanish</label>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="button" class="btn btn-primary" @click="go_next">Next</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="card-footer">
-                        <button class="btn btn-primary">Next</button>
+
+                    <div :id="stage.intro" class="w-100" :style="{display: current_stage === stage.intro ? 'block' : 'none'}">
+                        <div class="card bg-light mb-3">
+                            <div class="card-header">Intro</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Who do I have the pleasure of speaking to?</h5>
+                                <hr>
+                                <div class="form-group mb-4">
+                                    <label for="form_name">1. My name is</label>
+                                    <input type="text" name="form_name" id="form_name" class="form-control form-control-sm mx-3">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <label for="form_phn">2. What is the best contact number to reach you?</label>
+                                    <input type="text" name="form_phn" id="form_phn" class="form-control form-control-sm mx-3">
+                                </div>
+
+                                <div class="form-group">
+                                    <p class="mb-0 mr-3">3. Were you injured in a Car Accident or a “Slip & Fall”?</p>
+                                    <div class="custom-control custom-radio custom-control-inline">
+                                        <input type="radio" id="form_accident1" v-model="accident_type" value="ca" name="form_accident" class="custom-control-input" checked="checked">
+                                        <label class="custom-control-label" for="form_accident1">Car Accident</label>
+                                    </div>
+                                    <div class="custom-control custom-radio custom-control-inline">
+                                        <input type="radio" id="form_accident2" v-model="accident_type" value="sf" name="form_accident" class="custom-control-input">
+                                        <label class="custom-control-label" for="form_accident2">Slip & Fall</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="button" class="btn btn-primary" @click="go_next">Next</button>
+                            </div>
+                        </div>
                     </div>
-                </div>
+
+                    <div :id="stage.main" class="w-100" :style="{display: current_stage === stage.main ? 'block' : 'none'}">
+                        <div class="card bg-light mb-3">
+                            <div class="card-header">Questions</div>
+                            <template v-if="accident_type == 'ca'">
+                                <div class="card-body">
+                                    <h5 class="card-title">Car Accident</h5>
+                                    <hr>
+                                    Voila
+                                </div>
+                            </template>
+                            <template v-else-if="accident_type == 'sf'">
+                                <div class="card-body">
+                                    <h5 class="card-title">poira gesi ga</h5>
+                                    <hr>
+                                    gg
+                                </div>
+                            </template>
+                            <template v-else>
+                                <div class="card-body">
+                                    <div class="alert alert-warning">Please select Accident Type</div>
+                                </div>
+                                <div class="card-footer">
+                                    <button class="btn btn-info" type="button" @click="current_stage = stage.intro">Go Back</button>
+                                </div>
+                            </template>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,24 +1,51 @@
 var log = console.log;
 
-new Vue({
+var ques = new Vue({
     el: '#app',
-    
-    mounted: function(){
+    data: {
+        current_stage: "",
+        stage: {
+            lang: "stage-lang-ques",
+            intro: "stage-intro-ques",
+            main: "stage-main-ques"
+        },
+        accident_type: "",  // possible options: ca, sf; ca = car accident, sf = slip and fall
+    },
+    mounted: function () {
+        this.current_stage = this.stage.lang;
+        log("Current Stage: " + this.current_stage);
+
         $("input[data-picker='time']").timepicker({
             timeFormat: "hh:mm tt"
         });
         $("input[data-picker='date']").datepicker();
     },
     methods: {
-        before_form_submition: function(){
+        get_key_by_val(obj, val) {
+            return Object.keys(obj).find(key => obj[key] === val)
+        },
+        get_next_stage(){
+            let arr_of_keys = Object.keys(this.stage);
+            let i = arr_of_keys.indexOf(this.get_key_by_val(this.stage, this.current_stage));
+            return this.stage[arr_of_keys[i + 1]];
+        },
+        go_next() {
+            this.current_stage = this.get_next_stage();
+        },
+        before_form_submition() {
             var _t = this;
-            
+
             $('#submit-btn').addClass('disabled').children('img').removeClass('d-none').prev().addClass('d-none');
 
             $("#apmt_loc_name").val(_t.selected_location.name);
             $("#apmt_loc_address").val(_t.selected_location.address);
             $("#apmt_loc_available_time").val(_t.selected_location.available_time);
         },
-    }
+    },
+    watch: {
+        current_stage() {
+            $('#indicator-' + this.current_stage).removeClass('bg-transparent');
+        }
+    },
 });
 
